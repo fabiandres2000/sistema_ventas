9b63c6a3acabc729b09940200d34ed9856dc1bde
Andres Quintero <grovveip@gmail.com>
2023-12-18 18:07:54 -0500

    ok app para peso

--- public/ventas.bat
+++ public/ventas.bat
@@ -2,4 +2,6 @@
 cd C:\sistema_ventas
 
 REM Inicia el servidor Laravel
-start "" php artisan serve --host=192.168.1.23 --port=8000
+start "" php artisan serve --host 192.168.1.33 --port 8000
+
+start "" "C:\peso_aplicacion.exe"
